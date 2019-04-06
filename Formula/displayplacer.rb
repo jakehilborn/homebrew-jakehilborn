1becd2edb41866ed7a9d7d8b135e319a40c69368
jakehilborn <jakehilborn@gmail.com>
2019-04-06 16:35:59 -0700

    Fix displayplacer formula

--- Formula/displayplacer.rb
+++ Formula/displayplacer.rb
@@ -1,12 +1,11 @@
 class Displayplacer < Formula
   desc "macOS command line utility to configure multi-display resolutions and arrangements. Essentially XRandR for macOS."
   homepage "https://github.com/jakehilborn/displayplacer"
-  url "https://github.com/jakehilborn/displayplacer/releases/download/v1.0.0/displayplacer"
-  sha256 "9adf92acad4ff57c5df744f6f83260bb4318f4e29b8446e5123b4241f9d2d525"
-  version "1.0.0"
-  bottle :unneeded
+  url "https://github.com/jakehilborn/displayplacer/archive/v1.1.0.tar.gz"
+  sha256 "cb6ef09033ba5f36a89297c0b031f7a162b989884107499b97b4fc9710d8d5bc"
 
   def install
+    system "make"
     bin.install "displayplacer"
   end
 end
