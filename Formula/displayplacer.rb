51a07af8d493a733f811ba16fe0782a39b8c4ea9
jakehilborn <jakehilborn@gmail.com>
2019-05-17 15:48:50 -0700

    displayplacer v1.2.0

--- Formula/displayplacer.rb
+++ Formula/displayplacer.rb
@@ -1,8 +1,8 @@
 class Displayplacer < Formula
   desc "macOS command line utility to configure multi-display resolutions and arrangements. Essentially XRandR for macOS."
   homepage "https://github.com/jakehilborn/displayplacer"
-  url "https://github.com/jakehilborn/displayplacer/archive/v1.1.0.tar.gz"
-  sha256 "cb6ef09033ba5f36a89297c0b031f7a162b989884107499b97b4fc9710d8d5bc"
+  url "https://github.com/jakehilborn/displayplacer/archive/v1.2.0.tar.gz"
+  sha256 "95495b09f2b1a4cf63fafd8ea4a3ac916a8b652555966e1e81c1820c540df1f8"
 
   def install
     system "make"
